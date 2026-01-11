c23770a089b596cc34f2a10122bd313f262d70b6
chaitanya <chaitanyawade09@gmail.com>
2026-01-11 20:26:25 +0530

    learned about react router and implemnted it in ecommerce project

---
+++ react-js/react-js-course/practice-projects/ecommerce-project/src/App.jsx
@@ -0,0 +1,20 @@
+import { Routes, Route } from 'react-router'
+import { HomePage } from './pages/HomePage'
+import { Checkout } from './pages/Checkout'
+import { Orders } from './pages/Orders'
+import { Tracking } from './pages/Tracking'
+import './App.css'
+
+function App() {
+    return(<>
+        <Routes>
+            <Route index element={<HomePage />} />
+            <Route path='/checkout' element={<Checkout />} />
+            <Route path='/orders' element={<Orders />} />            
+            <Route path='/tracking' element={<Tracking />} />            
+        </Routes>
+        
+    </>)
+}
+
+export default App
